eb6f2ffda35d10c75b9dbaba896890bd763e2a05
sridhar0407 <=vsridhar77@gmail.com>
2025-04-24 11:51:09 +0530

    Completed AgData API implementation

--- src/main/java/com/agrichallenge/agdata/model/AgData.java
+++ src/main/java/com/agrichallenge/agdata/model/AgData.java
@@ -11,11 +11,59 @@ public class AgData {
 
     // TODO: Default constructor - No arguments
 
+    public AgData() {
+    }
 
     // TODO: Parameterized constructor
+
+    public AgData(Integer id, String crop, String region, Integer yield, Integer year) {
+        this.id = id;
+        this.crop = crop;
+        this.region = region;
+        this.yield = yield;
+        this.year = year;
+    }
 
 
     // TODO: Getters and Setters
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCrop() {
+        return crop;
+    }
+
+    public void setCrop(String crop) {
+        this.crop = crop;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public Integer getYield() {
+        return yield;
+    }
+
+    public void setYield(Integer yield) {
+        this.yield = yield;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
 }
